4ace9e0b3dd8717e6e290ecccbd358c2ac3bc247
Mostafa Abdelrashied <mostafa.abdelrashied@outlook.de>
2025-01-31 01:10:11 +0100

    add dvc pipeline

--- src/ml_project/data/data_loader.py
+++ src/ml_project/data/data_loader.py
@@ -1,3 +1,4 @@
+import json
 import os
 from pathlib import Path
 
@@ -29,8 +30,24 @@ class DataManager:
             logger.error(f"Data saving failed: {str(e)}")
             raise
 
+    def calculate_stats(self, df: pl.DataFrame, is_store: bool = False) -> dict:
+        # generate some stats about the data
+        stats_dict = {
+            "size": df.shape[0],
+            "num_features": df.shape[1],
+            "columns": df.columns,
+            "null_counts": df.null_count().sum_horizontal()[0],
+            "Quantity_mean": df["Quantity"].mean(),
+            "Quantity_std": df["Quantity"].std(),
+        }
+        if is_store:
+            with open("reports/data/data_stats.json", "w") as f:
+                json.dump(stats_dict, f)
+        return stats_dict
+
 
 if __name__ == "__main__":
     dl = DataManager()
     df = dl.load_raw_data()
-    print(df.head())
+    dl.save_processed_data(df, Path("data/processed/cleaned.parquet"))
+    dl.calculate_stats(df, is_store=True)
